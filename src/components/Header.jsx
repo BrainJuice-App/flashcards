bd384dffc7ba83b213ba09bd670863b774fae321
Alex Orlet <AlexOrlet89@gmail.com>
2022-05-26 14:24:54 -0700

    handler functions for logoug and signin/signup buttons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,37 @@
-import React from 'react';
+import { React } from 'react';
+import { useUser } from '../context/UserContext';
+import { useHistory, useLocation } from 'react-router-dom';
 
 export default function Header() {
+  const { user, logout } = useUser();
+  const history = useHistory();
+
+  const handleLogout = async () => {
+    await logout();
+  };
+
+  const handleRedirect = () => {
+    history.replace('/auth');
+  };
+
   return (
     <>
       <div>
         <h1>Header for our APP</h1>
         <div>
-          <button>SignIn/SignOut Button</button>
+          <div>
+            <h4>{user ? user.email : 'Unregistered Guest'}</h4>
+          </div>
+          {user ? (
+            <button onClick={() => handleLogout()}>Logout Button</button>
+          ) : (
+            <button onClick={() => handleRedirect()}>
+              Signin/Signup Button
+            </button>
+          )}
         </div>
       </div>
+      <hr />
     </>
   );
 }
