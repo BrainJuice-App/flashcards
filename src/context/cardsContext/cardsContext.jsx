6d5838a1b9756bf97d4f754c4a00eb3a89b762ff
Yovana Pelayo <yovana_pelayo@yahoo.com>
2022-05-26 12:17:40 -0700

    created card file

--- src/context/cardsContext/cardsContext.jsx
+++ src/context/cardsContext/cardsContext.jsx
@@ -1,36 +1,36 @@
-import { createContext, useContext, useState } from 'react';
+// import { createContext, useContext, useState } from 'react';
 
-export const CardContext = createContext();
+// export const CardContext = createContext();
 
-export const CardProvider = ({ children }) => {
-  const [cards, setCards] = useState('');
-  const [name, setName] = useState('');
-  const [content, setContent] = useState('');
-  const [review, setReview] = useState('');
-  const [loading, setLoading] = useState(true);
-  return (
-    <CardContext.Provider
-      value={{
-        cards,
-        setCards,
-        name,
-        setName,
-        content,
-        setContent,
-        review,
-        setReview,
-        loading,
-        setLoading,
-      }}
-    >
-      {children}
-    </CardContext.Provider>
-  );
-};
-export const useCard = () => {
-  const context = useContext(CardContext);
-  if (context === undefined) {
-    throw new Error('useUser must be used within a User Provider');
-  }
-  return context;
-};
+// export const CardProvider = ({ children }) => {
+//   const [cards, setCards] = useState('');
+//   const [name, setName] = useState('');
+//   const [content, setContent] = useState('');
+//   const [review, setReview] = useState('');
+//   const [loading, setLoading] = useState(true);
+//   return (
+//     <CardContext.Provider
+//       value={{
+//         cards,
+//         setCards,
+//         name,
+//         setName,
+//         content,
+//         setContent,
+//         review,
+//         setReview,
+//         loading,
+//         setLoading,
+//       }}
+//     >
+//       {children}
+//     </CardContext.Provider>
+//   );
+// };
+// export const useCard = () => {
+//   const context = useContext(CardContext);
+//   if (context === undefined) {
+//     throw new Error('useUser must be used within a User Provider');
+//   }
+//   return context;
+// };
--- src/services/cards.js
+++ src/services/cards.js
@@ -1,7 +1,7 @@
-// import { checkError, client } from './client.';
+import { checkError, client } from './client.';
 
-// export async function getCards() {
-//   const resp = await client.from('cards').select('*');
-//   console.log('resp', resp);
-//   return checkError(resp);
-// }
+export async function getCards() {
+  const resp = await client.from('cards').select('*');
+  console.log('resp', resp);
+  return checkError(resp);
+}
