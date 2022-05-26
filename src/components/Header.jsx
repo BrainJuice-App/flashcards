c25aa738389dff058438657f0174a39f77fc434f
Alex Orlet <AlexOrlet89@gmail.com>
2022-05-26 11:16:59 -0700

    Route already established by Kamie, moved code over to .jsx header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <>
+      <div>
+        <h1>Header for our APP</h1>
+        <div>
+          <button>SignIn/SignOut Button</button>
+        </div>
+      </div>
+    </>
+  );
 }
