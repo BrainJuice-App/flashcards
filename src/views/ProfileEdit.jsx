25c0b4f1047ee86cfb44e2efcbc02bb8d05f3240
Kamie Short <kamie.short@gmail.com>
2022-05-27 14:45:53 -0700

    added toast

--- src/views/ProfileEdit.jsx
+++ src/views/ProfileEdit.jsx
@@ -3,13 +3,15 @@ import ProfileForm from '../components/ProfileForm';
 import { updateProfile } from '../services/profile';
 import { useProfileContext } from '../context/ProfileContext';
 import { useUser } from '../context/UserContext';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+import { toast } from 'react-hot-toast';
 
 export default function ProfileEdit() {
   const { firstName, lastName, username, bio } = useProfileContext();
   const { user } = useUser();
+  const history = useHistory();
 
   const submitProfileHandler = async (e) => {
-    console.log('button clicked');
     e.preventDefault;
 
     const updates = {
@@ -20,6 +22,8 @@ export default function ProfileEdit() {
       bio: bio,
     };
     await updateProfile(updates);
+    history.push('/profile');
+    toast.success('You have successfully updated your profile!');
   };
 
   return (
