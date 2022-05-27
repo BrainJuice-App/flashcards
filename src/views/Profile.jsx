0fd84553e19492ff96dfd8f82bc6930dac3cc69e
Kamie Short <kamie.short@gmail.com>
2022-05-27 10:20:51 -0700

    working on profile display

--- src/views/Profile.jsx
+++ src/views/Profile.jsx
@@ -1,14 +1,38 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
+import { useProfileContext } from '../context/ProfileContext';
 import { useUser } from '../context/UserContext';
+import { getProfile } from '../services/profile';
+import { useEffect } from 'react';
 
 export default function Profile() {
   const history = useHistory();
   const { user } = useUser();
+  const {
+    firstName,
+    setFirstName,
+    lastName,
+    setLastName,
+    username,
+    setUsername,
+    email,
+    setEmail,
+    bio,
+    setBio,
+  } = useProfileContext();
 
   const handleEditButtonClick = () => {
     history.push(`/profile/${user.id}/edit`);
   };
+
+  useEffect(() => {
+    const fetchProfile = async () => {
+      const data = await getProfile(user.id);
+      console.log(data);
+      setFirstName(data.user.first_name);
+    };
+    fetchProfile();
+  }, []);
 
   return (
     <>
