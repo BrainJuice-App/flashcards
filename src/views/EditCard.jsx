12066f3a0cabbab4f64245b0495e3332809ad377
Alex Orlet <AlexOrlet89@gmail.com>
2022-05-31 16:06:05 -0700

    added params to card form, will continue when merged

--- src/views/EditCard.jsx
+++ src/views/EditCard.jsx
@@ -1,5 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom/cjs/react-router-dom.min';
+import CardForm from '../components/CardForm';
+import { getProfileCards } from '../services/cards';
 
 export default function EditCard() {
-  return <div>EditCard</div>;
+  const submitCreateHandler = () => {
+    console.log('click');
+  };
+  return (
+    //createHandler function that will update instead of create a card, we will probably need id from params to update the correct row in supabase
+
+    <>
+      <div>EditCard</div>
+      <CardForm submitCreateHandler={submitCreateHandler} />
+    </>
+  );
 }
