5666e28bd9c52713d5602ae1ea39cfed44c6b377
Yovana Pelayo <yovana_pelayo@yahoo.com>
2022-05-31 12:54:22 -0700

    established CArd.css

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
-
+import './Card.css';
 export default function Card() {
   return (
     <>
       <h2>Card</h2>
       <div className="card">
         <div className="card_inner">
-          <div className="card_face card_face--front"></div>
-          <div className="card_face card_face--back"></div>
+          <div className="card_face card_face--front">
+            <h2>Card Front</h2>
+          </div>
+          <div className="card_face card_face--back">
+            <div className="card_content">
+              <div className="card_header">
+                <img src="card.jpeg" alt="" className="pp" />
+                <h2>Question</h2>
+              </div>
+              <div className="card_body">
+                <h3>Answer</h3>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
