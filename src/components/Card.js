ef71a650ee77587e6def6a35c5294858f677c0b1
Kamie Short <kamie.short@gmail.com>
2022-05-31 16:30:45 -0700

    css on flip

--- src/components/Card.js
+++ src/components/Card.js
@@ -18,15 +18,6 @@ export default function Card() {
           </div>
           <div className={(styles.card_face, styles.card_faceBack)}>
             <div className={styles.card_content}>
-              {/* <div className={styles.card_header}>
-                <img
-                  src="../../public/card.jpeg"
-                  alt=""
-                  className={styles.pp}
-                />
-                <h2>Question</h2>
-              </div> */}
-
               <div className={styles.card_body}>
                 <h2 onClick={() => setActive(!isActive)}>Answer</h2>
               </div>
@@ -35,27 +26,5 @@ export default function Card() {
         </div>
       </div>
     </>
-
-    // <>
-    //   <h2>Card</h2>
-    //   <div className="card">
-    //     <div className="card_inner">
-    //       <div className="card_face card_face--front">
-    //         <h2>Card Front</h2>
-    //       </div>
-    //       <div className="card_face card_face--front">
-    //         <div className="card_face card_face--back">
-    //           <div className="card_content">
-    //             <img src="../../public/card.jpeg" alt="" className="pp" />
-    //             <h2>Question</h2>
-    //           </div>
-    //           <div className="card_body">
-    //             <h3>Answer</h3>
-    //           </div>
-    //         </div>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </>
   );
 }
