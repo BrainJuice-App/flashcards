fb1326bafa67e1c200b1808cb0a7bf5d8936b46f
Yovana Pelayo <yovana_pelayo@yahoo.com>
2022-06-02 12:57:24 -0700

    added profile picture and linked in

--- src/views/Dev.jsx
+++ src/views/Dev.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import kamie from '../assets/kamie.jpeg';
+import yovana from '../assets/yovana.jpeg';
 import styles from '../views/Dev.css';
 
 export default function Dev() {
@@ -28,9 +29,9 @@ export default function Dev() {
       <div>
         <h2>Yovana</h2>
         <p>Full Stack Software Engineer and I enjoy coding.</p>
-        <a href="https://www.linkedin.com/in/kamieshort/">
+        <a href="https://www.linkedin.com/in/yovana-pelayo/">
           <img
-            src={kamie}
+            src={yovana}
             style={{
               height: '250px',
               borderRadius: '8px',
@@ -42,7 +43,7 @@ export default function Dev() {
       <br />
       <br />
       <div>
-        <h2>Kamie</h2>
+        <h2>Yovana</h2>
         <p>Full Stack Software Engineer and I enjoy gardening and learning.</p>
         <a href="https://www.linkedin.com/in/kamieshort/">
           <img
